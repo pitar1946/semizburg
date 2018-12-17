a03242da179819a73333f3513101b0ccce6b5772
pitar1946 <bicoensar1@gmail.com>
2018-12-17 16:49:27 +0100

    second

--- src/components/OurBeers.js
+++ src/components/OurBeers.js
@@ -108,6 +108,45 @@ class OurBeers extends Component {
                     </div>
                 </div>
             </div>
+            <div className='col-md-4 md-5 mt-5'>
+            <div className="card">
+                    <div className="card-img-top mb-3">
+                        <img className='card-img' src="./images/beer1.png" alt="drekavac"/>
+                    </div>
+                    <div className="card-body">
+                        <h2>Pivo 7</h2>
+                        <p className="card-text mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-4 md-5 mt-5'>
+            <div className="card">
+                    <div className="card-img-top mb-3">
+                        <img className='card-img' src="./images/beer2.png" alt="drekavac"/>
+                    </div>
+                    <div className="card-body">
+                        <h2>Pivo 8</h2>
+                        <p className="card-text mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-4 md-5 mt-5'>
+            <div className="card">
+                    <div className="card-img-top mb-3">
+                        <img className='card-img' src="./images/beer3.png" alt="drekavac"/>
+                    </div>
+                    <div className="card-body">
+                        <h2>Pivo 9</h2>
+                        <p className="card-text mt-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                </div>
+            </div>
        </div>
        <p className='mt-3 info'> - IBU – Internacional Bitterness Unit</p>
        <p className='mb-3 info'>Označava hemijsku mjeru količine i broja gorkih sastojaka u pivu. Učestali podatak na etiketama zanatskih piva. Interesantno je da zapravo ne označava koliko je pivo gorko jer je balans ukusa ono što čini pravog Master Pivara.</p>
